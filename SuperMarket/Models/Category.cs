01b8813653d1d8adbcbe34f387e5e5477e4d6f11
prajal77 <prajal.basnet0@gmail.com>
2024-09-05 10:19:36 +0545

    ValidationAdded

--- SuperMarket/Models/Category.cs
+++ SuperMarket/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace SuperMarket.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarket.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; }= string.Empty;
 
